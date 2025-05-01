4cfb8094b1df358de8a6c7c88e2e27e4752462a5
MERIT100 <163988319+seokhoon100@users.noreply.github.com>
2025-05-01 18:37:09 +0900

    Add files via upload

---
+++ Week09/src/Week09/CalcEx.java
@@ -0,0 +1,25 @@
+package Week09;
+
+public class CalcEx {
+	public static void main(String[] args) {
+		Calculator c1 = new Calculator("white", "small");
+		System.out.println(c1.color + ", " + c1.size);
+		
+		int result1 = c1.plus(10, 20);
+		System.out.println("plus(10, 20) = " + result1);
+		
+		double result2 = c1.divide(10, 20);
+		System.out.println("divide(10, 20) = " + result2);
+		
+		byte b1 = 10;
+		byte b2 = 10;
+		result2 = c1.divide(b1, b2);
+		System.out.println("divide(10, 20) = " + result2);
+		
+		int sum1 = c1.sum1(new int[] {1, 2, 3, 4, 5});
+		System.out.println("{1, 2, 3, 4, 5} = " + sum1);
+		
+		int sum2 = c1.sum2(1, 2, 3, 4, 5);
+		System.out.println("(1, 2, 3, 4, 5) = " + sum2);
+	}
+}
